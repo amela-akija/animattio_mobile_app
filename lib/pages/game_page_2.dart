56d045397178ff762e355ffb1be8e60797d25bcf
Amela <aakija2002@gmail.com>
2024-08-10 15:12:36 +0200

    show random image for 250 ms with 1 s interval

--- lib/pages/game_page_2.dart
+++ lib/pages/game_page_2.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 
@@ -13,74 +14,129 @@ class GamePage2 extends StatefulWidget {
 }
 
 class _GamePage2State extends State<GamePage2> {
-  bool showImage = true;
-  int currentImage = 0;
-  int previousImage = 0;
-  late Timer imageTimer;
+  int currentImageIndex = 0;
+  int previousImageIndex = 0;
+
+  late Timer showImageTimer;
   late Timer hideImageTimer;
-  List<String> score = [];
-  List<String> shownImages = [];
+  bool isVisible = true;
 
   @override
   void initState() {
     super.initState();
-    _startImageDisplay();
+    showAndHideImage();
+    // showRandomImage();
+    // hideRandomImage();
   }
 
-  void _startImageDisplay() {
-    imageTimer = Timer.periodic(const Duration(seconds: 7), (Timer timer) {
-      setState(() {
-        showImage = true;
-      });
-      hideImageTimer = Timer(const Duration(seconds: 3), () {
-        setState(() {
-          currentImage = (currentImage + 1) % widget.listOfImages.length;
-          previousImage = currentImage;
-          shownImages.add(widget.listOfImages[previousImage]);
-          showImage = false;
-        });
-      });
+  void showAndHideImage(){
+      showImageTimer = Timer.periodic(Duration(milliseconds: 250), (timer) {
+      showRandomImage();
+    });
+
+    hideImageTimer = Timer.periodic(Duration(seconds: 2), (timer) {
+      hideRandomImage();
     });
   }
-  void _screenTap(){
-    if(widget.listOfImages[previousImage] == widget.stimuli){
-      score.add("f");
 
-    }else if(widget.listOfImages[previousImage] != widget.stimuli){
-      score.add("h");
-    }
+  void showRandomImage() {
+    // showImageTimer = Timer.periodic(const Duration(milliseconds: 250), (timer) {
+      Random random = Random();
+      setState(() {
+        currentImageIndex = random.nextInt(widget.listOfImages.length);
+        isVisible = false;
+      });
+    // });
   }
 
-  @override
+  void hideRandomImage() {
+    // Future.delayed(const Duration(seconds: 2), () {
+      if (this.mounted) {
+        setState(() {
+          isVisible = true;
+        });
+      }
+    // });
+  }
+
+    @override
   void dispose() {
-    imageTimer.cancel();
+    showImageTimer.cancel();
     hideImageTimer.cancel();
     super.dispose();
   }
+
+  void screenTap() {}
+  // bool showImage = true;
+  // int currentImage = 0;
+  // int previousImage = 0;
+  // late Timer imageTimer;
+  // late Timer hideImageTimer;
+  // List<String> score = [];
+  // List<String> shownImages = [];
+
+  // void _startImageDisplay() {
+  //   imageTimer = Timer.periodic(const Duration(seconds: 7), (Timer timer) {
+  //     setState(() {
+  //       showImage = true;
+  //     });
+  //     hideImageTimer = Timer(const Duration(seconds: 3), () {
+  //       setState(() {
+  //         currentImage = (currentImage + 1) % widget.listOfImages.length;
+  //         previousImage = currentImage;
+  //         shownImages.add(widget.listOfImages[previousImage]);
+  //         showImage = false;
+  //       });
+  //     });
+  //   });
+  // }
+  // void _screenTap(){
+  //   if(widget.listOfImages[previousImage] == widget.stimuli){
+  //     score.add("f");
+
+  //   }else if(widget.listOfImages[previousImage] != widget.stimuli){
+  //     score.add("h");
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
     //Colors
     Color pageColor = const Color(0xFFFEFFD9);
-
     return GestureDetector(
-      onTap: _screenTap,
+      onTap: screenTap,
       child: Scaffold(
         backgroundColor: pageColor,
-        body: Builder(
-          builder: (context) {
-            if(shownImages.length<5){
-            return Center(
-              child: showImage
-                  ? Image.asset(widget.listOfImages[currentImage])
-                  : const SizedBox.shrink(),
-            );}
-            else{
-              return Text(score.join(","));
-            }
-          }
+        body: Visibility(
+          maintainSize: true,
+          maintainAnimation: true,
+          maintainState: true,
+          visible: isVisible,
+          child: Center(
+            child: Image.asset(widget.listOfImages[currentImageIndex]),
+          ),
         ),
       ),
     );
+
+    // return GestureDetector(
+    //   onTap: _screenTap,
+    //   child: Scaffold(
+    //     backgroundColor: pageColor,
+    //     body: Builder(
+    //       builder: (context) {
+    //         if(shownImages.length<5){
+    //         return Center(
+    //           child: showImage
+    //               ? Image.asset(widget.listOfImages[currentImage])
+    //               : const SizedBox.shrink(),
+    //         );}
+    //         else{
+    //           return Text(score.join(","));
+    //         }
+    //       }
+    //     ),
+    //   ),
+    // );
   }
 }
